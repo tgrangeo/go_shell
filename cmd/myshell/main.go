0228213adc18eb4a21c17e5faa39a4e36523abc2
tgrangeo <thomas.grangeon9@gmail.com>
2024-06-10 13:40:25 +0200

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -10,6 +10,9 @@ func main() {
 	for{
 		fmt.Fprint(os.Stdout, "$ ")
 		cmd , _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		if (cmd == "exit 0\n"){
+			return
+		}
 		fmt.Println(cmd[:len(cmd)-1] +": command not found")
 	}
 }
